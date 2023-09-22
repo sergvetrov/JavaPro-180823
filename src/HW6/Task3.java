543cf32c2e2efc1651cae7ccd66a5360e3ab7d7d
Serhii Vietrov <sb.vetrov@gmail.com>
2023-09-22 14:06:20 +0300

    HW6 is done with additions methods

--- src/HW6/Task3.java
+++ src/HW6/Task3.java
@@ -1,5 +1,10 @@
 package HW6;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
 public class Task3 {
 
     private Task3() {
@@ -15,4 +20,43 @@ public class Task3 {
         }
         return null;
     }
+
+//    public static int[] twoSum(int[] nums, int target) {
+//        HashMap<Integer, Integer> collector = new HashMap<>();
+//        for (int number : nums) {
+//            if (collector.containsKey(number)) {
+//                List<Integer> ints = new ArrayList<>();
+//                for (int num : nums) {
+//                    ints.add(num);
+//                }
+//                return new int[]{ints.indexOf(number), ints.indexOf(collector.get(number))};
+//            } else {
+//                collector.put(target - number, number);
+//            }
+//        }
+//        return null;
+//    }
+
+//    public static int[] twoSum(int[] nums, int target) {
+//        int[] nums2 = Arrays.copyOf(nums, nums.length);
+//        Arrays.sort(nums);
+//        int left = 0;
+//        int right = nums.length - 1;
+//        while (nums[left] < nums[right]) {
+//            int total = nums[left] + nums[right];
+//            if (total > target) {
+//                right--;
+//            } else if (total < target) {
+//                left++;
+//            } else {
+//                List<Integer> ints = new ArrayList<>();
+//                for (int num : nums2) {
+//                    ints.add(num);
+//                }
+//                return new int[] {ints.indexOf(nums[left]), ints.indexOf(nums[right])};
+//            }
+//        }
+//        return null;
+//    }
+
 }
